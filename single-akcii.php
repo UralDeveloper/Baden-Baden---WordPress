fc8f18b4645e2f23367aeed8e145282622156d3a
Alexandr <80260446+UralDeveloper@users.noreply.github.com>
2025-07-07 21:52:17 +0500

    Update v1.6.20

--- single-akcii.php
+++ single-akcii.php
@@ -119,49 +119,44 @@ get_header();
 					}
 				</style>
 				<section class="specialOffers">
-					<!-- <div class=""> -->
-						<div class="titleBlock">
-							<h2>Другие акции</h2>
-						</div>
-						<div class="swiper specialOffers__wrapper-akcii">
-							<div class="swiper-wrapper">
-								<?php while ($query->have_posts()) : $query->the_post(); ?>
-									<div class="swiper-slide specialOffers__item swiper-slide-original specialOffers__item--theme_3">
-										<!-- Добавляем миниатюру поста -->
-										<div class="specialOffers__photo">
-											<?php the_post_thumbnail('full'); ?>
-										</div>
-										<div class="specialOffers__content">
-											<p class="specialOffers__content--title"><?php the_title(); ?></p>
-											<!-- noindex -->
-											<span class="category">
-												<?php
-													// Извлекаем название категории конкретной таксономии
-													$terms = get_the_terms(get_the_ID(), 'akcii_category');
-													if (!empty($terms) && !is_wp_error($terms)):
-														echo esc_html($terms[0]->name); // Первая категория из нужной таксономии
-													else:
-														echo 'Без категории';
-													endif;
-												?>
-											</span>
-											<!-- /noindex -->
-											<a href="<?php the_permalink(); ?>" class="btn btn-primary">Подробнее</a>
-										</div>
+					<div class="titleBlock">
+						<h2>Другие акции</h2>
+					</div>
+					<div class="swiper specialOffers__wrapper-akcii">
+						<div class="swiper-wrapper">
+							<?php while ($query->have_posts()) : $query->the_post(); ?>
+								<div class="swiper-slide specialOffers__item swiper-slide-original specialOffers__item--theme_3">
+									<div class="specialOffers__photo">
+										<?php the_post_thumbnail('full'); ?>
 									</div>
-								<?php endwhile; ?>
+									<div class="specialOffers__content">
+										<p class="specialOffers__content--title"><?php the_title(); ?></p>
+										<span class="category">
+											<?php
+												// Извлекаем название категории конкретной таксономии
+												$terms = get_the_terms(get_the_ID(), 'akcii_category');
+												if (!empty($terms) && !is_wp_error($terms)):
+													echo esc_html($terms[0]->name); // Первая категория из нужной таксономии
+												else:
+													echo 'Без категории';
+												endif;
+											?>
+										</span>
+										<a href="<?php the_permalink(); ?>" class="btn btn-primary">Подробнее</a>
+									</div>
+								</div>
+							<?php endwhile; ?>
+						</div>
+						<div class="swiper-nav">
+							<!-- Добавляем кнопки навигации -->
+							<div class="swiper-button-next">
+								<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="512" height="512" x="0" y="0" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><path d="M382.678 226.804 163.73 7.86C158.666 2.792 151.906 0 144.698 0s-13.968 2.792-19.032 7.86l-16.124 16.12c-10.492 10.504-10.492 27.576 0 38.064L293.398 245.9l-184.06 184.06c-5.064 5.068-7.86 11.824-7.86 19.028 0 7.212 2.796 13.968 7.86 19.04l16.124 16.116c5.068 5.068 11.824 7.86 19.032 7.86s13.968-2.792 19.032-7.86L382.678 265c5.076-5.084 7.864-11.872 7.848-19.088.016-7.244-2.772-14.028-7.848-19.108z" fill="#ffffff" opacity="1" data-original="#000000" class=""></path></g></svg>
 							</div>
-							<div class="swiper-nav">
-								<!-- Добавляем кнопки навигации -->
-								<div class="swiper-button-next">
-									<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="512" height="512" x="0" y="0" viewBox="0 0 492.004 492.004" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><path d="M382.678 226.804 163.73 7.86C158.666 2.792 151.906 0 144.698 0s-13.968 2.792-19.032 7.86l-16.124 16.12c-10.492 10.504-10.492 27.576 0 38.064L293.398 245.9l-184.06 184.06c-5.064 5.068-7.86 11.824-7.86 19.028 0 7.212 2.796 13.968 7.86 19.04l16.124 16.116c5.068 5.068 11.824 7.86 19.032 7.86s13.968-2.792 19.032-7.86L382.678 265c5.076-5.084 7.864-11.872 7.848-19.088.016-7.244-2.772-14.028-7.848-19.108z" fill="#ffffff" opacity="1" data-original="#000000" class=""></path></g></svg>
-								</div>
-								<div class="swiper-button-prev">
-									<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="512" height="512" x="0" y="0" viewBox="0 0 492 492" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><path d="M198.608 246.104 382.664 62.04c5.068-5.056 7.856-11.816 7.856-19.024 0-7.212-2.788-13.968-7.856-19.032l-16.128-16.12C361.476 2.792 354.712 0 347.504 0s-13.964 2.792-19.028 7.864L109.328 227.008c-5.084 5.08-7.868 11.868-7.848 19.084-.02 7.248 2.76 14.028 7.848 19.112l218.944 218.932c5.064 5.072 11.82 7.864 19.032 7.864 7.208 0 13.964-2.792 19.032-7.864l16.124-16.12c10.492-10.492 10.492-27.572 0-38.06L198.608 246.104z" fill="#ffffff" opacity="1" data-original="#000000" class=""></path></g></svg>
-								</div>
+							<div class="swiper-button-prev">
+								<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="512" height="512" x="0" y="0" viewBox="0 0 492 492" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><path d="M198.608 246.104 382.664 62.04c5.068-5.056 7.856-11.816 7.856-19.024 0-7.212-2.788-13.968-7.856-19.032l-16.128-16.12C361.476 2.792 354.712 0 347.504 0s-13.964 2.792-19.028 7.864L109.328 227.008c-5.084 5.08-7.868 11.868-7.848 19.084-.02 7.248 2.76 14.028 7.848 19.112l218.944 218.932c5.064 5.072 11.82 7.864 19.032 7.864 7.208 0 13.964-2.792 19.032-7.864l16.124-16.12c10.492-10.492 10.492-27.572 0-38.06L198.608 246.104z" fill="#ffffff" opacity="1" data-original="#000000" class=""></path></g></svg>
 							</div>
 						</div>
-					<!-- </div> -->
+					</div>
 				</section>
 			<?php endif; ?>
 			<?php wp_reset_postdata(); ?>
